a1d60093107bbf5a766ed456dfc72d45909a1eb6
SebinSong <songxevin@gmail.com>
2023-10-30 17:33:24 +0900

    add starlight theme with basic config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,25 @@
-import { defineConfig } from 'astro/config'
-import preact from '@astrojs/preact'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+import mdx from '@astrojs/mdx';
+
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
+  integrations: [
 		// Enable Preact to support Preact JSX components.
 		preact(),
-		mdx()
+		mdx(),
+		starlight({
+			title: 'Shelter protocol',
+		})
 	],
-	vite: {
-		esbuild: {
-			// below is an option added to silence the vite warning that is related to "/** @jsxImportSource preact */" in various .jsx files.
-			// (reference for a similar case: https://github.com/vitejs/vite-plugin-react/issues/12)
-			jsx: 'automatic'
-		}
-	},
-	site: 'https://shelterprotocol.net'
-})
+  vite: {
+    esbuild: {
+      // below is an option added to silence the vite warning that is related to "/** @jsxImportSource preact */" in various .jsx files.
+      // (reference for a similar case: https://github.com/vitejs/vite-plugin-react/issues/12)
+      jsx: 'automatic'
+    }
+  },
+  site: 'https://shelterprotocol.net'
+});
