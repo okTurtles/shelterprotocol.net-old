e1874d92c36957d9a286cf2a024a26e8085c4884
SebinSong <songxevin@gmail.com>
2023-11-03 10:58:51 +0900

    update the style of search modal and search-btn

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -20,12 +20,12 @@ export default function Search() {
 	return (
 		<>
 			<button type="button" onClick={openSearch} className="search-input">
-        <svg aria-label="Search" class="astro-gkc36pom astro-lq7oo3uf" width="16" height="16" viewBox="0 0 24 24"
+        <svg aria-label="Search" class="astro-gkc36pom astro-lq7oo3uf search-icon" width="16" height="16" viewBox="0 0 24 24"
           fill="currentColor" style="--sl-icon-size: 1em;">
             <path d="M21.71 20.29 18 16.61A9 9 0 1 0 16.61 18l3.68 3.68a.999.999 0 0 0 1.42 0 1 1 0 0 0 0-1.39ZM11 18a7 7 0 1 1 0-14 7 7 0 0 1 0 14Z"></path>
         </svg>
 
-				<span>Search</span>
+				<span class='text'>Search</span>
 
 				<svg aria-label="(Press / to Search)" class="sl-hidden md:sl-block astro-gkc36pom astro-lq7oo3uf" width="16" height="16" viewBox="0 0 24 24" fill="currentColor" style="--sl-icon-size: 1em;">
           <path d="M17 2H7a5 5 0 0 0-5 5v10a5 5 0 0 0 5 5h10a5 5 0 0 0 5-5V7a5 5 0 0 0-5-5Zm3 15a3 3 0 0 1-3 3H7a3 3 0 0 1-3-3V7a3 3 0 0 1 3-3h10a3 3 0 0 1 3 3v10Z"></path>
--- src/components/search/search-modal/SearchModal.jsx
+++ src/components/search/search-modal/SearchModal.jsx
@@ -77,7 +77,7 @@ function SearchModal ({
           noValidate={true}
           className={formElClass}>
           <label className='search-modal__input-label' htmlFor='search-input'>
-            <svg className='search-modal__search-icon' width="24" height="24" fill="none">
+            <svg className='search-modal__search-icon' width="18" height="18" viewBox='0 0 24 24' fill="none">
               <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                 stroke="currentColor"
                 strokeWidth="2"
